c9c7a8c3563a57f649293b268ff87b96ad65f396
allanlalangan <73400708+allanlalangan@users.noreply.github.com>
2022-05-15 21:31:28 -0700

    update ClassesGrid style. update Membership section

--- components/Membership/ClassesGrid.jsx
+++ components/Membership/ClassesGrid.jsx
@@ -1,18 +1,19 @@
+import Image from 'next/image';
 import styles from './ClassesGrid.module.css';
 
 const ClassesGrid = () => {
   return (
     <ul className={styles['classes-grid']}>
-      <li className={`${styles['class-card']}`}>
+      <li className={`${styles['class-card']} ${styles['boxing-card']}`}>
         <h2>Boxing</h2>
       </li>
-      <li className={`${styles['class-card']}`}>
+      <li className={`${styles['class-card']} ${styles['muaythai-card']}`}>
         <h2>Muay Thai</h2>
       </li>
-      <li className={`${styles['class-card']}`}>
+      <li className={`${styles['class-card']} ${styles['advanced-card']}`}>
         <h2>Advanced Classes</h2>
       </li>
-      <li className={`${styles['class-card']}`}>
+      <li className={`${styles['class-card']} ${styles['sparring-card']}`}>
         <h2>Sparring</h2>
       </li>
     </ul>
