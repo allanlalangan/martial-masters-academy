253fb2af19287889519d4ca65e5b343be1d203de
allanlalangan <73400708+allanlalangan@users.noreply.github.com>
2022-05-15 23:07:43 -0700

    update Hero styles. ClassCard saturate bgImage color onHover

--- components/Hero/Hero.jsx
+++ components/Hero/Hero.jsx
@@ -1,14 +1,21 @@
 import Link from 'next/link';
+import { GiKatana } from 'react-icons/gi';
 import styles from './Hero.module.css';
 
 const Hero = () => {
   return (
     <section className={`${styles.container} ${styles['container-layout']}`}>
       <div className={styles.banner}>
-        <h1 className={styles.heading}>Train different</h1>
+        <h2 className={styles.subheading}>Level up</h2>
+        <h1 className={styles.heading}>Your training</h1>
         <aside className={styles.tagline}>
-          <span>Hard work</span>
-          <span>Dedication</span>
+          <span>
+            A methodical{' '}
+            <span className={styles['icon-container']}>
+              <GiKatana className={styles['icon']} />
+            </span>
+            student-first approach to martial arts
+          </span>
         </aside>
       </div>
       <Link href='/about' passHref={true}>
--- components/Membership/ClassesGrid.jsx
+++ components/Membership/ClassesGrid.jsx
@@ -1,20 +1,19 @@
-import Image from 'next/image';
 import styles from './ClassesGrid.module.css';
 
 const ClassesGrid = () => {
   return (
     <ul className={styles['classes-grid']}>
       <li className={`${styles['class-card']} ${styles['boxing-card']}`}>
-        <h2>Boxing</h2>
+        <h2 className={styles['class-title']}>Boxing</h2>
       </li>
       <li className={`${styles['class-card']} ${styles['muaythai-card']}`}>
-        <h2>Muay Thai</h2>
+        <h2 className={styles['class-title']}>Muay Thai</h2>
       </li>
       <li className={`${styles['class-card']} ${styles['advanced-card']}`}>
-        <h2>Advanced Classes</h2>
+        <h2 className={styles['class-title']}>Advanced Classes</h2>
       </li>
       <li className={`${styles['class-card']} ${styles['sparring-card']}`}>
-        <h2>Sparring</h2>
+        <h2 className={styles['class-title']}>Sparring</h2>
       </li>
     </ul>
   );
