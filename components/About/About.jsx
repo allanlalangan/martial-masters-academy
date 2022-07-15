4ff50964eb0bf417077d7560dc946773f00670fa
allanlalangan <73400708+allanlalangan@users.noreply.github.com>
2022-07-15 10:22:17 -0700

    feat: create About component

---
+++ components/About/About.jsx
@@ -0,0 +1,24 @@
+import styles from './About.module.scss';
+
+const About = () => {
+  return (
+    <div>
+      <p>
+        We train athletes and individuals who are looking to achieve their
+        fitness goals, perfect their fighting skills, and protect themselves
+        from harm. We offer Martial Arts classes, Bootcamp classes, and Youth
+        Martial Arts classes that encourage proper form and doing your best.
+        From fighters to stay-at-home moms, we greet every member with a
+        welcoming, high-energy atmosphere to create the best experience here at
+        Martial Masters Academy. Our gym focuses on an intellectual approach to
+        fighting where we put emphasis on the smallest details and make the
+        biggest difference in one&apos;s combat skills. This has resulted in
+        making our gym a home for people who deeply desire to grow themselves
+        and challenge their body and mind everyday. Our coaches are devoted to
+        watching EVERY single member achieve their goals and become better
+        people in the gym and outside the gym.
+      </p>
+    </div>
+  );
+};
+export default About;
