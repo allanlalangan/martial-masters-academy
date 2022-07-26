f40a3af67547c52ac9b413bd1529dc1300a5b041
allanlalangan <73400708+allanlalangan@users.noreply.github.com>
2022-07-26 08:46:45 -0700

    feat: update Membership styles, init css grid

--- components/Membership/Membership.jsx
+++ components/Membership/Membership.jsx
@@ -2,44 +2,44 @@ import { FaCheckCircle } from 'react-icons/fa';
 
 const Membership = () => {
   return (
-    <section>
-      <article>
-        <div>
-          <h1>{`Monthly Membership`}</h1>
-          <h2>New members are eligilble for 3 free introductory classes!</h2>
-          <aside>
-            <h3>{`What's Included`}</h3>
-            <div></div>
-          </aside>
-          <ul>
-            <li>
-              <FaCheckCircle />
-              Full body workout in 60 minutes
-            </li>
-            <li>
-              <FaCheckCircle />
-              Unlimited access to all classes
-            </li>
-            <li>
-              <FaCheckCircle />
+    <section className='grid grid-cols-12 grid-rows-6 p-8'>
+      <article className='rounded-l-lg lg:col-span-6 lg:row-span-6 bg-slate-700 p-4'>
+        <h1>{`Monthly Membership`}</h1>
+        <h2>New members are eligilble for 3 free introductory classes!</h2>
+        <aside>
+          <h3>{`What's Included`}</h3>
+          <div className='h-[1px] bg-primary-700 mt-2 mb-4'></div>
+        </aside>
+        <ul className='grid grid-cols-12 grid-rows-6'>
+          <li className='lg:col-span-6 lg:row-span-3 flex'>
+            <FaCheckCircle className='text-emerald-400' />
+            <span className='pl-2'>Full body workout in 60 minutes</span>
+          </li>
+          <li className='lg:col-span-6 lg:row-span-3 flex'>
+            <FaCheckCircle className='text-emerald-400' />
+            <span className='pl-2'>Unlimited access to all classes</span>
+          </li>
+          <li className='lg:col-span-6 lg:row-span-3 flex'>
+            <FaCheckCircle className='text-emerald-400' />
+            <span className='pl-2'>
               Personalized training with our coaches!
-            </li>
-            <li>
-              <FaCheckCircle />
-              Refer-a-friend rewards
-            </li>
-          </ul>
-        </div>
+            </span>
+          </li>
+          <li className='lg:col-span-6 lg:row-span-3 flex'>
+            <FaCheckCircle className='text-emerald-400' />
+            <span className='pl-2'>Refer-a-friend rewards</span>
+          </li>
+        </ul>
+      </article>
+      <article className='rounded-r-lg lg:col-span-6 lg:row-span-6 bg-slate-600 p-4'>
+        <h2>Join us for a fixed price of</h2>
+        <h1>
+          <span>$</span>99.99
+        </h1>
         <div>
-          <h2>Join us for a fixed price of</h2>
-          <h1>
-            <span>$</span>99.99
-          </h1>
-          <div>
-            <span>Learn more</span>
-            <button>Contact a Coach</button>
-            <button>Visit Us</button>
-          </div>
+          <span>Learn more</span>
+          <button>Contact a Coach</button>
+          <button>Visit Us</button>
         </div>
       </article>
     </section>
