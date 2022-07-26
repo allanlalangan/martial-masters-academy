869a0835e93bcba509370223db64fa66757460bf
allanlalangan <73400708+allanlalangan@users.noreply.github.com>
2022-07-25 21:53:11 -0700

    feat(styles): extend default height util class

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 module.exports = {
   content: ['./pages/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      height: {
+        header: '13vh',
+        main: '87vh',
+      },
+    },
   },
   plugins: [],
 };
