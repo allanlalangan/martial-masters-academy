7c6e7e413f6c1d569ce6f0dc452218539de52534
allanlalangan <73400708+allanlalangan@users.noreply.github.com>
2022-07-27 10:39:26 -0700

    feat: update Features and Membership styles and responsive design

--- components/Features/Features.jsx
+++ components/Features/Features.jsx
@@ -3,35 +3,55 @@ import { FaAward } from 'react-icons/fa';
 
 const FeaturesGrid = () => {
   return (
-    <ul className='grid grid-cols-12 grid-rows-6 gap-4 px-8'>
-      <li className='bg-slate-700 lg:col-span-3 lg:row-span-6 p-8 rounded-lg '>
-        <MdTimer className='text-5xl' />
-        <h2>Full body workout in 60 minutes</h2>
-        <p>
+    <ul className='bg-gray-800 grid grid-cols-12 gap-4 p-8 my-12'>
+      <li className='relative grid grid-cols-12 grid-rows-5 bg-gray-700 h-full col-span-12 md:col-span-6 lg:col-span-3 p-8 rounded-lg rounded-tl-sm rounded-br-sm rounded-tr-lg rounded-bl-lg pt-16 shadow-md'>
+        <figure className='absolute bg-gray-900 left-[1rem] top-[-1rem] p-4 w-fit rounded-tl-sm rounded-br-sm rounded-tr-lg rounded-bl-lg shadow-lg'>
+          <MdTimer className='text-5xl' />
+        </figure>
+        <h2 className='flex items-center col-span-12 row-span-1 font-bold pt-4'>
+          Full workout in 60 minutes
+        </h2>
+        <div className='col-span-12 row-span-1 w-full h-[1px] bg-primary-700 my-auto'></div>
+        <p className='col-span-12 row-span-3'>
           From boxing to kickboxing, we mix cardio and resistance training for a
           full body workout in 60 minutes.
         </p>
       </li>
-      <li className='bg-slate-700 lg:col-span-3 lg:row-span-6 p-8 rounded-lg '>
-        <MdAllInclusive className='text-5xl' />
-        <h2>Access to ALL classes</h2>
-        <p>
+      <li className='relative grid grid-cols-12 grid-rows-5 bg-gray-700 h-full col-span-12 md:col-span-6 lg:col-span-3 p-8 rounded-lg rounded-tl-sm rounded-br-sm rounded-tr-lg rounded-bl-lg pt-16 shadow-md'>
+        <figure className='absolute bg-gray-900 left-[1rem] top-[-1rem] p-4 w-fit rounded-tl-sm rounded-br-sm rounded-tr-lg rounded-bl-lg shadow-lg'>
+          <MdAllInclusive className='text-5xl' />
+        </figure>
+        <h2 className='flex items-center col-span-12 row-span-1 font-bold pt-4'>
+          Access to ALL classes
+        </h2>
+        <div className='col-span-12 row-span-1 w-full h-[1px] bg-primary-700 my-auto'></div>
+        <p className='col-span-12 row-span-3'>
           You can take any and every class we offer throughout the week. Want to
           box? Come in. Want to do two a days? Hop in! Grow as fast as you want!
         </p>
       </li>
-      <li className='bg-slate-700 lg:col-span-3 lg:row-span-6 p-8 rounded-lg '>
-        <FaAward className='text-5xl' />
-        <h2>Refer-a-friend rewards</h2>
-        <p>
+      <li className='relative grid grid-cols-12 grid-rows-5 bg-gray-700 h-full col-span-12 md:col-span-6 lg:col-span-3 p-8 rounded-lg rounded-tl-sm rounded-br-sm rounded-tr-lg rounded-bl-lg pt-16 shadow-md'>
+        <figure className='absolute bg-gray-900 left-[1rem] top-[-1rem] p-4 w-fit rounded-tl-sm rounded-br-sm rounded-tr-lg rounded-bl-lg shadow-lg'>
+          <FaAward className='text-5xl' />
+        </figure>
+        <h2 className='flex items-center col-span-12 row-span-1 font-bold pt-4'>
+          Refer-a-friend rewards
+        </h2>
+        <div className='col-span-12 row-span-1 w-full h-[1px] bg-primary-700 my-auto'></div>
+        <p className='col-span-12 row-span-3'>
           We know it{"'"}s not easy to train by yourself. Bring in a friend with
           you to class and receive a free private session with our coaches.
         </p>
       </li>
-      <li className='bg-slate-700 lg:col-span-3 lg:row-span-6 p-8 rounded-lg '>
-        <MdTimer className='text-5xl' />
-        <h2>Private Training</h2>
-        <p>
+      <li className='relative grid grid-cols-12 grid-rows-5 bg-gray-700 h-full col-span-12 md:col-span-6 lg:col-span-3 p-8 rounded-lg rounded-tl-sm rounded-br-sm rounded-tr-lg rounded-bl-lg pt-16 shadow-md'>
+        <figure className='absolute bg-gray-900 left-[1rem] top-[-1rem] p-4 w-fit rounded-tl-sm rounded-br-sm rounded-tr-lg rounded-bl-lg shadow-lg'>
+          <MdTimer className='text-5xl' />
+        </figure>
+        <h2 className='flex items-center col-span-12 row-span-1 font-bold pt-4'>
+          Private Training
+        </h2>
+        <div className='col-span-12 row-span-1 w-full h-[1px] bg-primary-700 my-auto'></div>
+        <p className='col-span-12 row-span-3'>
           Whether your goals are to compete, get fit, or learn martial arts, our
           coaches are ready to help you reach them.
         </p>
--- components/Membership/Membership.jsx
+++ components/Membership/Membership.jsx
@@ -2,44 +2,58 @@ import { FaCheckCircle } from 'react-icons/fa';
 
 const Membership = () => {
   return (
-    <section className='grid grid-cols-12 grid-rows-6'>
-      <article className='rounded-tr-lg lg:rounded-r-none lg:rounded-bl-lg col-span-12 row-span-3 lg:col-span-6 lg:row-span-6 bg-slate-700 p-4'>
-        <h1>{`Monthly Membership`}</h1>
-        <h2>New members are eligilble for 3 free introductory classes!</h2>
-        <aside>
-          <h3>{`What's Included`}</h3>
-          <div className='h-[1px] bg-primary-700 mt-2 mb-4'></div>
+    <section className='grid grid-cols-12 grid-rows-6 m-8'>
+      <article className='flex flex-col justify-evenly rounded-tr-lg lg:rounded-r-none lg:rounded-bl-lg col-span-12 row-span-3 lg:col-span-8 lg:row-span-6 bg-gray-800 p-12 shadow-md'>
+        <h3 className='text-3xl font-bold tracking-wide'>Monthly Membership</h3>
+        <h3 className='text-lg tracking-wide'>
+          New members are eligible for 3 free introductory classes!
+        </h3>
+        <aside className='flex items-center my-4'>
+          <h3 className='w-[55%] tracking-widest uppercase text-sm text-center'>{`What's Included`}</h3>
+          <div className='w-full h-[1px] bg-primary-700'></div>
         </aside>
-        <ul className='grid grid-cols-12 grid-rows-6'>
-          <li className='col-span-12 row-span-3 lg:col-span-6 lg:row-span-3 flex'>
+        <ul className='grid grid-cols-12 gap-2'>
+          <li className='col-span-12 lg:col-span-6 flex justify-start items-center w-full'>
             <FaCheckCircle className='text-emerald-400' />
-            <span className='pl-2'>Full body workout in 60 minutes</span>
+            <span className='text-sm pl-2'>
+              Full body workout in 60 minutes
+            </span>
           </li>
-          <li className='col-span-12 row-span-3 lg:col-span-6 lg:row-span-3 flex'>
+          <li className='col-span-12 lg:col-span-6 flex justify-start items-center w-full'>
             <FaCheckCircle className='text-emerald-400' />
-            <span className='pl-2'>Unlimited access to all classes</span>
+            <span className='text-sm pl-2'>
+              Unlimited access to all classes
+            </span>
           </li>
-          <li className='col-span-12 row-span-3 lg:col-span-6 lg:row-span-3 flex'>
+          <li className='col-span-12 lg:col-span-6 flex justify-start items-center w-full'>
             <FaCheckCircle className='text-emerald-400' />
-            <span className='pl-2'>
+            <span className='text-sm pl-2'>
               Personalized training with our coaches!
             </span>
           </li>
-          <li className='col-span-12 row-span-3 lg:col-span-6 lg:row-span-3 flex'>
+
+          <li className='col-span-12 lg:col-span-6 flex justify-start items-center w-full'>
             <FaCheckCircle className='text-emerald-400' />
-            <span className='pl-2'>Refer-a-friend rewards</span>
+            <span className='text-sm pl-2'>Refer-a-friend rewards</span>
           </li>
         </ul>
       </article>
-      <article className='rounded-bl-lg lg:rounded-l-none lg:rounded-tr-lg col-span-12 row-span-3 lg:col-span-6 lg:row-span-6 bg-slate-600 p-4'>
-        <h2>Join us for a fixed price of</h2>
-        <h1>
-          <span>$</span>99.99
-        </h1>
-        <div>
-          <span>Learn more</span>
-          <button>Contact a Coach</button>
-          <button>Visit Us</button>
+      <article className='flex flex-col items-center rounded-bl-lg lg:rounded-l-none lg:rounded-tr-lg col-span-12 row-span-3 lg:col-span-4 lg:row-span-6 bg-gray-700 p-12'>
+        <h2 className='p-1 text-xl font-semibold tracking-wide'>
+          Join us for a fixed price of
+        </h2>
+        <h2 className='text-6xl font-extrabold flex items-start tracking-wide'>
+          <span className='text-4xl h-full my-auto'>$</span>99.99
+          <span className='font-light text-2xl'>USD</span>
+        </h2>
+        <div className='w-full flex flex-col items-center'>
+          <span className='underline text-sm p-2'>Learn more</span>
+          <button className='w-[75%] rounded-bl-lg rounded-br-sm rounded-tr-lg rounded-tl-sm bg-gray-800 text-sm px-8 py-4 hover:brightness-110 my-2 mx-auto transition tracking-widest'>
+            Contact a Coach
+          </button>
+          <button className='w-[75%] rounded-bl-lg rounded-br-sm rounded-tr-lg rounded-tl-sm bg-gray-800 text-sm px-8 py-4 hover:brightness-110 my-2 mx-auto transition tracking-widest'>
+            Visit Us
+          </button>
         </div>
       </article>
     </section>
