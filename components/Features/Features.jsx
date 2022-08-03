82e32dab6da8f151546d6eacd58d8db701f28acf
allanlalangan <73400708+allanlalangan@users.noreply.github.com>
2022-08-03 14:47:58 -0700

    fix(Features): adjust x margin to avoid overflow

--- components/Features/Features.jsx
+++ components/Features/Features.jsx
@@ -3,54 +3,54 @@ import { FaAward, FaDumbbell } from 'react-icons/fa';
 
 const FeaturesGrid = () => {
   return (
-    <ul className='pattern bg-gray-800 z-0 grid grid-cols-12 gap-8 p-8 lg:px-16 2xl:mx-36 my-12'>
-      <li className='relative grid grid-cols-12 grid-rows-4 bg-gray-700 h-full col-span-12 md:col-span-6 lg:col-span-3 p-8 border-theme pt-16 shadow-md'>
-        <figure className='absolute bg-gray-900 left-[1rem] top-[-1rem] p-4 w-fit rounded-tl-sm rounded-br-sm rounded-tr-lg rounded-bl-lg shadow-lg'>
+    <ul className='pattern bg-gray-800 z-0 grid grid-cols-12 gap-8 p-8 lg:px-16 my-12'>
+      <li className='features-card'>
+        <figure className='features-icon border-theme shadow-lg'>
           <MdTimer className='text-5xl' />
         </figure>
         <h2 className='pt-6 text-xl flex items-baseline col-span-12 row-span-1 font-semibold'>
           Full workout in 60 minutes
         </h2>
-        <div className='col-span-12 row-span-1 w-full h-[1px] bg-primary-700 my-auto'></div>
+        <div className='features-divider'></div>
         <p className='leading-relaxed col-span-12 row-span-2 font-light'>
           From boxing to kickboxing, we mix cardio and resistance training for a
           full body workout in 60 minutes.
         </p>
       </li>
-      <li className='relative grid grid-cols-12 grid-rows-4 bg-gray-700 h-full col-span-12 md:col-span-6 lg:col-span-3 p-8 border-theme pt-16 shadow-md'>
-        <figure className='absolute bg-gray-900 left-[1rem] top-[-1rem] p-4 w-fit rounded-tl-sm rounded-br-sm rounded-tr-lg rounded-bl-lg shadow-lg'>
+      <li className='features-card'>
+        <figure className='features-icon border-theme shadow-lg'>
           <MdAllInclusive className='text-5xl' />
         </figure>
         <h2 className='pt-6 text-xl flex items-baseline col-span-12 row-span-1 font-semibold'>
           Access to ALL classes
         </h2>
-        <div className='col-span-12 row-span-1 w-full h-[1px] bg-primary-700 my-auto'></div>
+        <div className='features-divider'></div>
         <p className='leading-relaxed col-span-12 row-span-2 font-light'>
           You can take any and every class we offer throughout the week. Want to
           box? Come in. Want to do two a days? Hop in! Grow as fast as you want!
         </p>
       </li>
-      <li className='relative grid grid-cols-12 grid-rows-4 bg-gray-700 h-full col-span-12 md:col-span-6 lg:col-span-3 p-8 border-theme pt-16 shadow-md'>
-        <figure className='absolute bg-gray-900 left-[1rem] top-[-1rem] p-4 w-fit rounded-tl-sm rounded-br-sm rounded-tr-lg rounded-bl-lg shadow-lg'>
+      <li className='features-card'>
+        <figure className='features-icon border-theme shadow-lg'>
           <FaAward className='text-5xl' />
         </figure>
         <h2 className='pt-6 text-xl flex items-baseline col-span-12 row-span-1 font-semibold'>
           Refer-a-friend rewards
         </h2>
-        <div className='col-span-12 row-span-1 w-full h-[1px] bg-primary-700 my-auto'></div>
+        <div className='features-divider'></div>
         <p className='leading-relaxed col-span-12 row-span-2 font-light'>
-          We know it{"'"}s not easy to train by yourself. Bring in a friend with
-          you to class and receive a free private session with our coaches.
+          We know it&apos;s not easy to train by yourself. Bring in a friend
+          with you to class and receive a free private session with our coaches.
         </p>
       </li>
-      <li className='relative grid grid-cols-12 grid-rows-4 bg-gray-700 h-full col-span-12 md:col-span-6 lg:col-span-3 p-8 border-theme pt-16 shadow-md'>
-        <figure className='absolute bg-gray-900 left-[1rem] top-[-1rem] p-4 w-fit rounded-tl-sm rounded-br-sm rounded-tr-lg rounded-bl-lg shadow-lg'>
+      <li className='features-card'>
+        <figure className='features-icon border-theme shadow-lg'>
           <FaDumbbell className='text-5xl' />
         </figure>
         <h2 className='pt-6 text-xl flex items-baseline col-span-12 row-span-1 font-semibold'>
           Private Training
         </h2>
-        <div className='col-span-12 row-span-1 w-full h-[1px] bg-primary-700 my-auto'></div>
+        <div className='features-divider'></div>
         <p className='leading-relaxed col-span-12 row-span-2 font-light'>
           Whether your goals are to compete, get fit, or learn martial arts, our
           coaches are ready to help you reach them.
