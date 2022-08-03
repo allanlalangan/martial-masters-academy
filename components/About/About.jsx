5f355335311343c2baa5049de4105c4343ef211c
allanlalangan <73400708+allanlalangan@users.noreply.github.com>
2022-08-02 18:31:47 -0700

    feat: add Hero and About images

--- components/About/About.jsx
+++ components/About/About.jsx
@@ -1,10 +1,13 @@
+import Image from 'next/image';
+import aboutImage from '../../public/assets/marvin-cors-FBnlCT-cGUQ-unsplash.jpeg';
+
 const About = () => {
   return (
-    <section className='grid grid-cols-12 grid-rows-4 lg:h-screen 2xl:mx-36'>
-      <figure className='col-span-12 row-span-3 md:col-start-7 md:col-end-13 md:row-start-1 md:row-end-5 flex justify-center items-center'>
-        image
+    <section className='grid grid-cols-12 grid-rows-4 lg:h-screen p-8 2xl:mx-36'>
+      <figure className='p-4 flex align-center col-span-12 row-span-3 md:col-start-7 md:col-end-13 md:row-start-1 md:row-end-5'>
+        <Image className='object-cover' src={aboutImage} alt='' />
       </figure>
-      <article className='p-8 leading-relaxed col-span-12 row-span-3 md:col-start-1 md:col-end-7 md:row-start-1 md:row-end-5 flex justify-center items-center'>
+      <article className='leading-relaxed col-span-12 row-span-3 md:col-start-1 md:col-end-7 md:row-start-1 md:row-end-5 flex justify-center items-center'>
         We train athletes and individuals who are looking to achieve their
         fitness goals, perfect their fighting skills, and protect themselves
         from harm. We offer Martial Arts classes, Bootcamp classes, and Youth
--- components/Hero/Hero.jsx
+++ components/Hero/Hero.jsx
@@ -1,15 +1,16 @@
+import Image from 'next/image';
 import Link from 'next/link';
 import { GiKatana } from 'react-icons/gi';
-
+import heroImage from '../../public/assets/luis-vidal-FodEsaNZs48-unsplash.jpeg';
 const Hero = () => {
   return (
-    <section className='h-main flex flex-col justify-center items-center border-b border-slate-700'>
-      <div className='flex flex-col justify-center items-center'>
-        <h2 className='tracking-wider'>Level up</h2>
-        <h1 className='tracking-wider font-black uppercase text-7xl py-4'>
+    <section className='relative grid grid-cols-12 h-main'>
+      <div className='col-span-6 p-16 flex flex-col justify-center items-start z-40 bg-gray-800'>
+        <h2 className='tracking-wider drop-shadow-sm'>Level up</h2>
+        <h1 className='tracking-wider font-black uppercase text-7xl py-4 drop-shadow-lg'>
           Your training
         </h1>
-        <aside>
+        <aside className='drop-shadow-sm'>
           <span className='flex tracking-wide'>
             A methodical
             <span className='px-1'>
@@ -18,10 +19,13 @@ const Hero = () => {
             student-first approach to martial arts
           </span>
         </aside>
+        <Link href='/about' passHref={true}>
+          <button className='action-button'>About Us</button>
+        </Link>
       </div>
-      <Link href='/about' passHref={true}>
-        <button className='action-button'>About Us</button>
-      </Link>
+      <figure className='object-cover overflow-hidden col-span-6flex items-center absolute w-full h-full right-0'>
+        <Image src={heroImage} alt='gym' />
+      </figure>
     </section>
   );
 };
