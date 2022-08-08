d1592aa3c455f57a6aa151a221c18d14be022f11
allanlalangan <73400708+allanlalangan@users.noreply.github.com>
2022-08-07 22:12:42 -0700

    feat: update CoachCard styles
    rename divider util class

--- components/CoachCard/CoachCard.jsx
+++ components/CoachCard/CoachCard.jsx
@@ -1,29 +1,42 @@
+import portrait from '../../public/assets/marvin-cors-FBnlCT-cGUQ-unsplash.jpeg';
 import { BsInstagram } from 'react-icons/bs';
+import Image from 'next/image';
+import { CgShapeRhombus } from 'react-icons/cg';
 
 const CoachCard = () => {
   return (
-    <article>
-      <li className='pattern flex flex-col items-start justify-center bg-gray-700 p-8'>
-        <h2 className=''>Name</h2>
-        <figure className='border-theme border border-primary-700 bg-gray-700 p-4'>
-          Portrait
-        </figure>
-        <h2 className=''>Head Coach</h2>
-        <ul className='flex items-center justify-evenly'>
-          <li className='mr-4'>Boxing</li>
-          <li className='mr-4'>Muay Thai</li>
-          <li className='mr-4'>Boot Camp</li>
-        </ul>
-        <p className=''>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Culpa, ex
-          sit! Illo aspernatur magni, vel asperiores cumque pariatur vero
-          delectus eius saepe ipsam labore.
-        </p>
+    <li className='border-theme grid-rows-[repeat(6, 1fr)] col-span-12 grid grid-cols-12 gap-4 bg-gray-700 p-8 sm:col-span-6 lg:col-span-12'>
+      <article className='col-span-12 row-start-1 row-end-2 flex items-center justify-between'>
+        <h2 className='text-2xl font-bold tracking-wide'>First Last</h2>
         <aside className=''>
-          <BsInstagram />
+          <BsInstagram className='text-3xl' />
         </aside>
-      </li>
-    </article>
+      </article>
+      <figure className='border-theme col-span-12 row-start-2 row-end-3 flex h-[50vh] bg-gray-700 p-4'>
+        <Image src={portrait} className='object-cover' alt='' />
+      </figure>
+      {/* <ul className='col-span-12 row-start-3 row-end-4 flex flex-col items-start justify-evenly'>
+        <li className='mr-4 flex items-center'>
+          <CgShapeRhombus className='text-lg text-emerald-400' />
+          <span className='pl-2'>Boxing</span>
+        </li>
+        <li className='mr-4 flex items-center'>
+          <CgShapeRhombus className='text-lg text-emerald-400' />
+          <span className='pl-2'>Muay Thai</span>
+        </li>
+        <li className='mr-4 flex items-center'>
+          <CgShapeRhombus className='text-lg text-emerald-400' />
+          <span className='pl-2'>Boot Camp</span>
+        </li>
+      </ul> */}
+      <div className='divider'></div>
+      <aside className='col-span-12 row-start-4 row-end-5'>Head Coach</aside>
+      <p className='col-span-12 row-start-5 row-end-7'>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Culpa, ex sit!
+        Illo aspernatur magni, vel asperiores cumque pariatur vero delectus eius
+        saepe ipsam labore.
+      </p>
+    </li>
   );
 };
 export default CoachCard;
--- pages/instructors.js
+++ pages/instructors.js
@@ -5,8 +5,13 @@ import { BsInstagram } from 'react-icons/bs';
 const Instructors__page = () => {
   return (
     <>
-      <h1>Meet Our Coaches</h1>
-      <ul className=''>
+      <h2 className='mx-8 mb-12 border-b-[1.5px] border-gray-700 p-12 pb-8 text-center text-3xl font-bold uppercase tracking-widest lg:mx-16'>
+        Meet Our Coaches
+      </h2>
+      <ul className='mx-8 grid grid-cols-12 gap-8'>
+        <CoachCard />
+        <CoachCard />
+        <CoachCard />
         <CoachCard />
       </ul>
     </>
