2bda7e9461923c1b1527be4e792c1b127b25f9b3
allanlalangan <73400708+allanlalangan@users.noreply.github.com>
2022-08-10 17:46:42 -0700

    feat(instructors): map CoachCards
    update page composition

--- components/CoachCard/CoachCard.jsx
+++ components/CoachCard/CoachCard.jsx
@@ -1,19 +1,38 @@
-import portrait from '../../public/assets/marvin-cors-FBnlCT-cGUQ-unsplash.jpeg';
-import { BsInstagram } from 'react-icons/bs';
+import portrait from '../../public/assets/johann-walter-bantz-HG1pkXN7SVA-unsplash.jpg';
+import { BsInstagram, BsFacebook } from 'react-icons/bs';
 import Image from 'next/image';
 import { CgShapeRhombus } from 'react-icons/cg';
 
-const CoachCard = () => {
+const CoachCard = ({ coach, index }) => {
+  const even = index % 2 == 0;
   return (
-    <li className='border-theme grid-rows-[repeat(6, 1fr)] col-span-12 grid grid-cols-12 gap-4 bg-gray-700 p-8 sm:col-span-6 lg:col-span-12'>
-      <article className='col-span-12 row-start-1 row-end-2 flex items-center justify-between'>
-        <h2 className='text-2xl font-bold tracking-wide'>First Last</h2>
-        <aside className=''>
-          <BsInstagram className='text-3xl' />
-        </aside>
+    <li className='pattern border-theme col-span-12 grid h-full grid-cols-12 gap-4 bg-gray-800 p-8 md:grid-rows-6 lg:col-span-12'>
+      <article className='border-theme col-span-12 row-span-1 flex items-center justify-between border-b-2 border-primary-700 bg-gray-800'>
+        <section className='p-4'>
+          <h2 className='text-2xl font-bold uppercase tracking-wider'>
+            {coach.name}
+          </h2>
+          <aside className='col-span-12 row-span-1'>{coach.title}</aside>
+        </section>
+        <section className='flex h-full flex-col md:flex-row'>
+          <button className='border-theme h-full rounded-bl-none bg-gray-700 p-4 transition hover:brightness-110 md:rounded-tr-none'>
+            <BsInstagram className='text-3xl' />
+          </button>
+          <button className='md:border-theme h-full bg-gray-700 p-4 transition hover:brightness-110 md:rounded-bl-none'>
+            <BsFacebook className='text-3xl' />
+          </button>
+        </section>
       </article>
-      <figure className='border-theme col-span-12 row-start-2 row-end-3 flex h-[50vh] bg-gray-700 p-4'>
-        <Image src={portrait} className='object-cover' alt='' />
+      <figure
+        className={`border-theme ${
+          even ? 'section-left' : 'section-right'
+        } col-span-12 row-span-3 flex`}
+      >
+        <Image
+          src={portrait}
+          className='border-theme h-fit object-cover'
+          alt=''
+        />
       </figure>
       {/* <ul className='col-span-12 row-start-3 row-end-4 flex flex-col items-start justify-evenly'>
         <li className='mr-4 flex items-center'>
@@ -29,12 +48,13 @@ const CoachCard = () => {
           <span className='pl-2'>Boot Camp</span>
         </li>
       </ul> */}
-      <div className='divider'></div>
-      <aside className='col-span-12 row-start-4 row-end-5'>Head Coach</aside>
-      <p className='col-span-12 row-start-5 row-end-7'>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Culpa, ex sit!
-        Illo aspernatur magni, vel asperiores cumque pariatur vero delectus eius
-        saepe ipsam labore.
+      {/* <div className='divider col-span-12'></div> */}
+      <p
+        className={`border-theme ${
+          even ? 'section-right' : 'section-left'
+        } col-span-12 row-span-2 flex items-start border-t-2 border-primary-700 bg-gray-800 p-4 pt-4 md:border-none`}
+      >
+        {coach.bio}
       </p>
     </li>
   );
--- pages/instructors.js
+++ pages/instructors.js
@@ -1,6 +1,7 @@
 import CoachCard from '../components/CoachCard/CoachCard';
-
+import { coaches } from '../public/static/data';
 import { BsInstagram } from 'react-icons/bs';
+import Divider from '../components/Divider/Divider';
 
 const Instructors__page = () => {
   return (
@@ -8,12 +9,12 @@ const Instructors__page = () => {
       <h2 className='mx-8 mb-12 border-b-[1.5px] border-gray-700 p-12 pb-8 text-center text-3xl font-bold uppercase tracking-widest lg:mx-16'>
         Meet Our Coaches
       </h2>
-      <ul className='mx-8 grid grid-cols-12 gap-8'>
-        <CoachCard />
-        <CoachCard />
-        <CoachCard />
-        <CoachCard />
+      <ul className='mx-8 mb-12 grid grid-cols-12 gap-8 md:mx-16 lg:mx-32'>
+        {coaches.map((coach, i) => (
+          <CoachCard key={i} index={i} coach={coach} />
+        ))}
       </ul>
+      <Divider />
     </>
   );
 };
