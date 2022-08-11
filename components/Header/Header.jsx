8d5c6c8c90bce8294bb0432f97f539c32ea1cb89
allanlalangan <73400708+allanlalangan@users.noreply.github.com>
2022-08-10 17:49:01 -0700

    feat(nav): add navOpen state
    add transition to nav

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -1,17 +1,22 @@
 import Link from 'next/link';
 import { useRouter } from 'next/router';
+import { useState } from 'react';
 import { FaCalendarAlt } from 'react-icons/fa';
 
 import { GiStarShuriken } from 'react-icons/gi';
 import { MdMenu } from 'react-icons/md';
 
 const Header = () => {
+  const [navOpen, setNavOpen] = useState(false);
+  const toggleNav = () => setNavOpen(!navOpen);
   const router = useRouter();
-  console.log(router.pathname);
   return (
-    <header className='relative flex h-header items-center justify-between px-8 2xl:mx-36'>
+    <header className='fixed top-0 z-30 flex h-header w-full items-center justify-between bg-gray-900 px-8 shadow-md 2xl:mx-36'>
       <Link href='/' passHref={true}>
-        <div className='flex cursor-pointer items-center justify-center transition hover:brightness-110 '>
+        <div
+          onClick={() => setNavOpen(false)}
+          className='flex cursor-pointer items-center justify-center transition hover:brightness-110 '
+        >
           <h1 className='flex flex-col font-[Mirza] uppercase'>
             <span className='text-xl leading-4 tracking-[.275rem]'>
               Martial
@@ -23,11 +28,16 @@ const Header = () => {
           </h1>
         </div>
       </Link>
-      <nav className='fixed top-0 left-[-50%] z-20 h-screen overflow-hidden bg-gray-800 bg-opacity-90 md:static md:h-fit md:bg-transparent'>
-        <ul className='flex h-full flex-col items-center justify-evenly text-sm md:flex-row'>
+      <nav
+        className={`fixed top-0 mt-[13vh] w-full ${
+          navOpen ? 'left-0 opacity-100' : 'left-[-100%] opacity-0'
+        } transition-nav z-10 h-full overflow-hidden bg-gray-800 md:static md:mt-0 md:h-fit md:w-fit md:bg-transparent md:opacity-100`}
+      >
+        <ul className='flex h-full flex-col items-center justify-start text-sm md:flex-row'>
           <Link href='/instructors' passHref={true}>
             <li
-              className={`cursor-pointer px-4 tracking-wider transition hover:brightness-110 ${
+              onClick={() => setNavOpen(false)}
+              className={`mx-2 my-4 cursor-pointer px-2 tracking-wider transition hover:brightness-110 ${
                 router.pathname === '/instructors' && 'current-page'
               }`}
             >
@@ -35,26 +45,25 @@ const Header = () => {
             </li>
           </Link>
           <li
+            onClick={() => setNavOpen(false)}
             className={
-              'cursor-pointer px-4 tracking-wider transition hover:brightness-110'
+              'mx-2 my-4 cursor-pointer px-2 tracking-wider transition hover:brightness-110'
             }
           >
-            Classes
+            Programs
           </li>
           <li
-            className={
-              'cursor-pointer px-4 tracking-wider transition hover:brightness-110'
-            }
+            onClick={() => setNavOpen(false)}
+            className='mx-2 my-4 flex cursor-pointer items-center px-2 tracking-wider transition hover:brightness-110'
           >
-            Youth Program
-          </li>
-          <li className='flex cursor-pointer items-center px-4 tracking-wider transition hover:brightness-110'>
             <span>Class Schedule</span>
             <FaCalendarAlt className='text-xltext-emerald-400 ml-2 text-emerald-400' />
           </li>
         </ul>
       </nav>
-      <MdMenu className='text-2xl md:hidden' />
+      <button className='md:hidden' onClick={toggleNav}>
+        <MdMenu className='text-2xl md:hidden' />
+      </button>
     </header>
   );
 };
