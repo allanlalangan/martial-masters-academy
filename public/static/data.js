dace2bd4d459c41f3981cdd8f145a6eeb53afedc
allanlalangan <73400708+allanlalangan@users.noreply.github.com>
2022-08-10 17:45:29 -0700

    feat(public): update coaches data

---
+++ public/static/data.js
@@ -0,0 +1,61 @@
+export const brand = {
+  name: 'Martial Masters Academy',
+};
+
+export const hero = {};
+
+export const coaches = [
+  {
+    name: "Kaiwi 'Eevie' Amina",
+    title: 'Head Trainer',
+    bio: 'Founder and Head Trainer Eevie has been studying and training Martial Arts for over a decade. Both an avid teacher and student of the game, his philosophy focuses on details and nuances of the game. Classes teach fundamentals while allowing students to flow and have fun. Eevie has trained a stable of fighters in both amateur and professional competition.',
+  },
+  {
+    name: 'Kalani Amina',
+    title: 'Head Trainer',
+    bio: "Kalani's passion for Martial Arts training came when he was faced with a medical emergency. Having been diagnosed diabetic at the age of 16, doctors told him that he would not live past his 20th birthday. He still stands today, losing over 100 pounds. Through Muay Thai and Boxing, he is eager to help others to promote a healthy lifestyle and achieve their goals.",
+  },
+  {
+    name: 'Rebecca Spaulding',
+    title: 'Personal Trainer',
+    bio: 'Rebecca is our fitness instructor from Chapel Hill, North Carolina! She came to Oregon to study health sciences and it inspired her to become a personal trainer and health and fitness instructor. Alongside fitness, Rebecca has spent the last 6 years studying and training boxing. Her specialty is creating a high intensity workout that will get you sweating from start to finish.',
+  },
+  {
+    name: 'Fernando Alvarado',
+    title: 'Assistant Coach',
+    bio: 'Fernando has been competing in professional mixed martial arts for over 5 years. Fernando is also an entertainer and brings the same energy on the mic. Bars in the studio, armbars in the cage. A proud father and husband, Fernando assists coach Kalani in our youth program.',
+  },
+];
+
+export const classes = [
+  {
+    name: 'Boxing',
+    description:
+      'Boxing is a combat sport utilizing only two hands to strike and your whole body to create different angles of attack and defense. This class focuses on basic boxing techniques from footwork to basic punching combinations.',
+  },
+  {
+    name: 'Muay Thai',
+    description:
+      'This ancient war art focuses on using the whole body as a weapon to throw devastating blows to the enemy or opponent. Muay Thai will teach the basic fundamentals of striking, defending with the use of hands, shins/feet, knees, and elbows.',
+  },
+  {
+    name: 'Advanced Boxing',
+    description:
+      'This class focuses on advanced tactics, technique, and strategies that include advanced mitt work, inside fighting, southpaw tactics, advanced footwork and angles. Students who have trained 6 or more months of boxing are welcomed.',
+  },
+  {
+    name: 'Advanced Muay Thai',
+    description:
+      'Advanced Muay Thai focuses on advanced tactics, technique, and strategies that include clinch tactics, sweeps, counter, advanced fakes and setups. There is also light technical sparring involved. Students who have trained 6 or more months of muay thai are welcomed.',
+  },
+  {
+    name: 'Boot Camp Fitness',
+    description:
+      'Our Bootcamp program is an intense full body fitness system that is guaranteed to produce results. We focus on proper form and functionality of the human body through intense training that includes; muscular strength, HIIT, cardiovascular endurance, speed and agility.',
+  },
+  {
+    name: 'Sparring',
+    description:
+      'Our Heavy bag program welcomes Boxers, Kickboxers, and MMA Athletes of all levels. Bag class gives you an intense cardiovascular workout while training your basic striking combos. ',
+  },
+];
--- static/content.js
+++
@@ -1,5 +0,0 @@
-export const brand = {
-  name: 'Martial Masters Academy',
-};
-
-export const hero = {};
